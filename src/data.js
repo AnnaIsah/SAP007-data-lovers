5590e058e72314c09cbe4b839f0e274118d03d94
moreirathais <thais_mjs@hotmail.com>
2022-02-23 10:53:28 -0300

    inserção dos cards na tela

--- src/data.js
+++ src/data.js
@@ -1,14 +1,14 @@
-export const filterData = (dataGhibli, condition) => {
-  if (condition == "filme 1") {
-      return film.films[0].title
-  }
-}
+//export const filterData = (dataGhibli, condition) => {
+ // if (condition == "filme 1") {
+ //     return film.films[0].title
+ // }
+//}
 
 
 
 
 
-//const filterData = (data, prop, name) => {
-//const filterData = data.filter(data => data[prop] === name)
-//  return filterData;
-//};
+export const filterData = (dataGhibli, prop, name) => {
+const filterData = dataGhibli.filter(dataGhibli => dataGhibli[prop] === name)
+  return filterData;
+};
--- src/main.js
+++ src/main.js
@@ -1,33 +1,57 @@
 import dataGhibli from "./data/ghibli/ghibli.js"
 import { filterData } from "./data.js"
 
-let resposta = document.getElementById("resposta");
+//PARA CHAMAR A ARRAY
+const films = dataGhibli.films;
+
+
+//PARA FILTRAR OS FILMES
+films.map (films => films.id);
+console.log(films);
+
+//PARA PEGAR O VALOR DO INPUT DO SELETOR
+director.addEventListener("change", () => {
+  const optionsDirector = document.getElementById("director")
+  const valueDirector = optionsDirector.options[optionsDirector.selectedIndex].text;
+
+  console.log("oi");
+});
+ 
+
+for (let i = 0; i < films.length; i++) {
+
+    const cardSection = document.createElement("div")
+    const titleElement = document.createElement("h2")
+    const posterElement = document.createElement("img")
+    let primeiroItem = ""
+    primeiroItem = films[i];
+    posterElement.src = primeiroItem.poster
+    titleElement.innerHTML = primeiroItem.title
+    cardSection.appendChild(titleElement)
+    cardSection.appendChild(posterElement)
+    const card = document.getElementById("card")
+    card.appendChild(cardSection)
+}
+
+//for (let i = 0; i <= films.length; i++) {
+ //   let filtraDiretor = films[i].director;
+  //  if(director.filterData){
+ //       cardSection
+
+ //   }
+   // console.log(films[i]);
+//}
+
+
+
+
+// let resposta = document.getElementById("resposta");
 
 //let film1 = dataGhibli.filterData(0, "filme 1");
 
 //resposta.innerHTML = film1;
 
 
-
-console.log(dataGhibli.films.title);
-
-const films = dataGhibli.films
-for (let i = 0; i <= films.length; i++) {
-    console.log(films[i]);
-}
-
-
-//////////////////////////////////////////////////////
-
-//let filtro = data.map(data=>data.id);
-
-//console.log(filtro);
-
 ////////////////////////////////////////////////////////////////////
 
 
-//let director = document.querySelector(".director");
-
-//director.addEventListener("click", () => {
-//  director.value = filterData(data, director);
-//});
